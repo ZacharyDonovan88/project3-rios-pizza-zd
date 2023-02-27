35dd63b5588db709cf8cde545541356c1f41ed59
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2023-02-27 14:53:36 +1030

    navbar style update, added capture images

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,49 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import "../App.css";
 import "./css/navbar.css";
 
-
-
 function Home() {
-
-
   return (
-    <>
-      <nav>
-        <ul>
-            <li>
-            <Link to="/pizza">Pizza</Link>
-            </li>
-            <li>
-            <Link to="/Sides">Sides</Link>
-            </li>  
-            <li>
-            <Link to="/Drinks">Drinks</Link>
-            </li>  
-            <li>
-            <Link to="/Desserts">Desserts</Link>
-            </li>  
-            <li>
-            <Link to="/Deals">Deals</Link>
-            </li>
-            <li className="logSign">
-            <Link to="/Login">Log In</Link>
-            </li>
-            <li className="logSign">
-            <Link to="/Sign Up">Sign Up</Link>
-            </li>   
-        </ul>
-      </nav>
-    </>
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/pizza" activeClassName="active">
+            Pizza
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/sides" activeClassName="active">
+            Sides
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/drinks" activeClassName="active">
+            Drinks
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/desserts" activeClassName="active">
+            Desserts
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/deals" activeClassName="active">
+            Deals
+          </NavLink>
+        </li>
+        <li className="logSign">
+          <NavLink exact to="/login" activeClassName="active">
+            Log In
+          </NavLink>
+        </li>
+        <li className="logSign">
+          <NavLink exact to="/signup" activeClassName="active">
+            Sign Up
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
